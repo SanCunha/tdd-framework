71dc699e478672ebe3ed9cdb6231451fb129736b
San Cunha <san.silva@aluno.ufop.edu.br>
2024-07-25 22:07:14 -0300

    correct code testSuiteController

--- src/controllers/testSuiteController.ts
+++ src/controllers/testSuiteController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
-import { testSuites, TestSuite } from '../models/testSuite';
+import { TestSuite, TestCase } from '../models/testSuite';
 import { TestSuiteService } from '../services/testSuiteService';
 
 const testSuiteService = new TestSuiteService();
